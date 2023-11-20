3d573039aea02df37f87d00946d686ed18b0591f
Miler <estudos.miler@gmail.com>
2023-11-20 18:50:38 -0300

    primeiro commit

---
+++ src/componentes/MenuLink/index.jsx
@@ -0,0 +1,20 @@
+import { Link, NavLink, useLocation } from "react-router-dom"
+import styles from './MenuLink.module.css'
+
+
+const MenuLink = ({children, to}) => {
+    // const localizacao = useLocation();
+    return (
+        <NavLink className={({isActive}) => `
+            ${styles.link}
+            ${isActive ? styles.linkDestacado : ""}
+        `} 
+            to={to}
+            end
+            >
+                {children}
+        </NavLink>
+    )
+}
+
+export default MenuLink
---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,15 @@
+import styles from "../Rodape/Rodape.module.css"
+// import   { ReactComponent as MarcaRegistrada } from '../../assets/marca_registrada.svg'
+
+const Rodape = () => {
+    return (
+        <footer className={styles.rodape}>
+            {/* <MarcaRegistrada/> */}
+
+            Desenvolvido por Miler.
+            
+        </footer>
+    )
+}
+
+export default Rodape
